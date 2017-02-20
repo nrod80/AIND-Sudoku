4de12429dfd47a494863562403f30f35c99663fa
Nick Rodriguez <nick@catalytic.com>
2017-02-19 19:48:42 -0600

    diagonal sudoku

--- utils.py
+++ utils.py
@@ -4,12 +4,16 @@ cols = '123456789'
 def cross(a, b):
     return [s+t for s in a for t in b]
 
+def diagonal(a, b):
+    assert(len(a) == len(b))
+    return [a[idx]+b[idx] for idx in range(len(a))]
 
 boxes = cross(rows, cols)
 
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+diagonal_units = [diagonal(rows, cols), diagonal(rows, '987654321')]
+unitlist = row_units + column_units + square_units + diagonal_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
